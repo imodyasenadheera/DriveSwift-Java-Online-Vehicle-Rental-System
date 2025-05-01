b7336e1506bd9dce376f37b0c50b553d48ff40db
Imodya Senadheera <imodyapamadi@gmail.com>
2025-05-01 18:27:30 +0530

    DB name change

--- src/main/java/com/driveswift/util/ConnectionData.java
+++ src/main/java/com/driveswift/util/ConnectionData.java
@@ -2,7 +2,7 @@ package com.driveswift.util;
 
 public class ConnectionData {
 
-    public static final String DATABASE = "flexiride_db";
+    public static final String DATABASE = "driveswift_db";
     public static final String USERNAME = "root";
     public static final String PASSWORD = "123456";
 }
